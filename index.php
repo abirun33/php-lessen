0ccc64bdd8ab869ce0dd7b81d723eb13dde2f0e9
abiru <abirun33@gmail.com>
2020-02-21 15:01:26 +0900

    cc

--- index.php
+++ index.php
@@ -73,6 +73,7 @@ if($_SERVER['REQUEST_METHOD']==='POST'){
         <div class="alert alert-primary" role="alert">
            <p>投稿内容<?=$message->get_main() ?></p>
            <p class="text-right"><?=$message->get_user_name() ?> (<?=$mesaage->get_created_at() ?> )投稿者</p>
+           
         </div>
       <?php } ?>  
     </div>
